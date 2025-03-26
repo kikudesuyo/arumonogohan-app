e1b1dcb4449da88a86983abf8f3ed36c057b32a8
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-03-27 00:19:41 +0900

    refactor: useragent振り分け

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -24,55 +25,60 @@ func main() {
 }
 
 func postCallback(c *gin.Context) {
-	lineBot, err := service.NewLineBotClient(store)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	events, err := lineBot.Bot.ParseRequest(c.Request)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	message, err := lineBot.GetMessage(events)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	userID, err := lineBot.GetUserID(events)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	session, err := store.Get(userID)
-	if err != nil {
-		fmt.Println("session not found. creating new session")
-		session = &entity.ChatHistory{Messages: []string{}, State: "menu_select", Timestamp: time.Now()}
-		store.Save(userID, message, session.State)
-	}
-
-	replayParams := &service.LineReplyParams{
-		LineBot: lineBot,
-		UserID:  userID,
-		Message: message,
-		Session: session,
-		Store:   store,
-		Events:  events,
-	}
-
-	switch {
-	case session.State == "menu_select":
-		err = service.ReplyMenuSelect(replayParams)
+	userAgent := c.GetHeader("User-Agent")
+	// LINE Botクライアントの場合
+	if strings.Contains(userAgent, "LineBotWebhook") {
+		lineBot, err := service.NewLineBotClient(store)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-	case session.State == "ingredient_input":
-		err = service.ReplyIngredientInput(replayParams)
+		events, err := lineBot.Bot.ParseRequest(c.Request)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
+		message, err := lineBot.GetMessage(events)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		userID, err := lineBot.GetUserID(events)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		session, err := store.Get(userID)
+		if err != nil {
+			fmt.Println("session not found. creating new session")
+			session = &entity.ChatHistory{Messages: []string{}, State: "menu_select", Timestamp: time.Now()}
+			store.Save(userID, message, session.State)
+		}
+
+		replayParams := &service.LineReplyParams{
+			LineBot: lineBot,
+			UserID:  userID,
+			Message: message,
+			Session: session,
+			Store:   store,
+			Events:  events,
+		}
+
+		switch {
+		case session.State == "menu_select":
+			err = service.ReplyMenuSelect(replayParams)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		case session.State == "ingredient_input":
+			err = service.ReplyIngredientInput(replayParams)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		}
+		return
 	}
 }
